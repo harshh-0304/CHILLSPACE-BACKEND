ebc267b3c4ae1627e4b31bf41d503481162a58d3
harshh <harshjadav332004@gmail.com>
2025-03-10 20:31:04 +0530

    full workingbackend

---
+++ src/models/UserModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+
+    firstName:{
+        type:String
+    },
+    lastName:{
+        type:String
+    },
+    age:{
+        type:Number
+    },
+    status:{
+        type:Boolean,
+        default:true
+    },
+    roleId:{
+        type:Schema.Types.ObjectId, //batugasoijkadsasiksaj
+        ref:"roles"
+    },
+    password:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true
+    }
+
+})
+
+module.exports = mongoose.model("users",userSchema)
---
+++ src/routes/UserRoutes.js
@@ -0,0 +1,16 @@
+//router
+const routes = require("express").Router()
+//controller --> userController
+const userController = require("../controllers/UserController")
+//routes.post("/user",userController.addUser)
+routes.post("/user",userController.signup)
+routes.get("/users",userController.getAllUsers)
+routes.get("/user/:id",userController.getUserById)
+routes.delete("/user/:id",userController.deleteUserById)
+routes.post("/user/login",userController.loginUser)
+//get
+//post
+//delete
+//get
+
+module.exports = routes
